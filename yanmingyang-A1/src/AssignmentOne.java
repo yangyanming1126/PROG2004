0660513d00a01d8ab7d1f3ebc023964128b19cf7
yangyanming1126 <y.yang.70@student.scu.edu.au>
2024-07-06 18:01:45 +0800

    1

--- yanmingyang-A1/src/AssignmentOne.java
+++ yanmingyang-A1/src/AssignmentOne.java
@@ -1,2 +1,11 @@
 public class AssignmentOne {
+    public static void main(String[] args){
+        HealthProfessional hp1 = new HealthProfessional(1,"Alex","eyes");
+        hp1.printDetails();
+
+        System.out.println("----------------");
+
+        HealthProfessional hp2 = new HealthProfessional();
+        hp2.printDetails();
+    }
 }
--- yanmingyang-A1/src/HealthProfessional.java
+++ yanmingyang-A1/src/HealthProfessional.java
@@ -1,2 +1,41 @@
 public class HealthProfessional {
+    private int id;
+    private String name;
+    private String department;
+
+    public HealthProfessional(){
+        this.id = 123456;
+        this.name = "Alex";
+        this.department = "eyes";
+
+    }
+    public HealthProfessional(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+    public void printDetails(){
+        System.out.println("ID:" + id);
+        System.out.println("Name: " + name);
+        System.out.println("Departmment: " + department);
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId() {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
