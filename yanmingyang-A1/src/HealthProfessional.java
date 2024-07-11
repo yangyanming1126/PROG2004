8b707f2c6f219f90138fa25d57cc6ab35049db75
yangyanming1126 <y.yang.70@student.scu.edu.au>
2024-07-11 22:14:14 +0800

    improveHP

--- yanmingyang-A1/src/HealthProfessional.java
+++ yanmingyang-A1/src/HealthProfessional.java
@@ -3,39 +3,55 @@ public class HealthProfessional {
     private String name;
     private String department;
 
-    public HealthProfessional(){
+    public HealthProfessional() {
         this.id = 123456;
         this.name = "Alex";
         this.department = "eyes";
 
     }
+
     public HealthProfessional(int id, String name, String department) {
         this.id = id;
         this.name = name;
         this.department = department;
     }
-    public void printDetails(){
+
+    public void printDetails() {
         System.out.println("ID:" + id);
         System.out.println("Name: " + name);
-        System.out.println("Departmment: " + department);
+        System.out.println("Department: " + department);
     }
+
     public int getId() {
         return id;
     }
+
     public void setId() {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDepartment() {
         return department;
     }
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "HealthProfessional{" +
+                "id=" + id +
+                ", name='" + name +'\'' +
+                ", department='" + department + '\'' +
+                '}';
     }
 }
