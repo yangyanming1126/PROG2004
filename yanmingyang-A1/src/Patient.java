04a2f3377166502dedec568307bdd8ccd0fb6194
yangyanming1126 <y.yang.70@student.scu.edu.au>
2024-07-11 23:06:03 +0800

    improve Patient

--- yanmingyang-A1/src/Patient.java
+++ yanmingyang-A1/src/Patient.java
@@ -1,2 +1,40 @@
 public class Patient {
+    public String patientName;
+    private String patientMobile;
+
+
+    public Patient() {
+    }
+
+    public Patient(String patientName, String patientMobile) {
+        this.patientName = patientName;
+        this.patientMobile = patientMobile;
+    }
+
+
+
+    public String getPatientName() {
+        return patientName;
+
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientMobile() {
+        return patientMobile;
+    }
+
+    public void setPatientMobile(String patientMobile) {
+        this.patientMobile = patientMobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientName='" + patientName + '\'' +
+                ", patientMobile='" + patientMobile + '\'' +
+                '}';
+    }
 }
