dc82ff2f000bb4b59225941c7229100c700ccc3d
yangyanming1126 <y.yang.70@student.scu.edu.au>
2024-07-11 22:38:39 +0800

    improveAppointment

--- yanmingyang-A1/src/Appointment.java
+++ yanmingyang-A1/src/Appointment.java
@@ -1,58 +1,55 @@
-public class Appointment {
-    private String patientName;
-    private String patientMobile;
-    private String preferredTimeSlot;
-    private HealthProfessional doctor;
+import java.time.LocalDateTime;
 
-    public Appointment(){
-        this.patientName = "Annie";
-        this.patientMobile = "+86 13147712718";
-        this.preferredTimeSlot = "14:30";
-        this.doctor = null;
+    /**
+     * 创建一个类来容纳患者预约
+     */
+    public class Appointment {
+        private Patient patient;
+
+        /**
+         * 预约时间
+         */
+        public String timeOfAppointment;
+
+        /**
+         * 选择的医生
+         */
+        private HealthProfessional doctor;
+
+        public Appointment() {
+            this.patient = new Patient("Annie", "13147712718");
+            this.doctor = null;
+        }
+
+        public Appointment(Patient patient, String timeOfAppointment, HealthProfessional doctor) {
+            this.patient = patient;
+            this.timeOfAppointment = timeOfAppointment;
+            this.doctor = doctor;
+        }
+
+        public void printDetails() {
+            System.out.println(this.patient);
+            System.out.println("预约时间："+this.timeOfAppointment);
+            if (doctor != null) {
+                doctor.printDetails();
+            } else {
+                System.out.println("Doctor: Not assigned");
+            }
+        }
+
+        public Patient getPatient() {
+            return patient;
+        }
+
+        public void setPatient(Patient patient) {
+            this.patient = patient;
+        }
+
+        public HealthProfessional getDoctor() {
+            return doctor;
+        }
+
+        public void setDoctor(HealthProfessional doctor) {
+            this.doctor = doctor;
+        }
     }
-
-    public Appointment(String patientName, String patientMobile, String preferredTimeSlot,HealthProfessional doctor){
-    this.patientName = patientName;
-    this.patientMobile = patientMobile;
-    this.preferredTimeSlot = preferredTimeSlot;
-    this.doctor = doctor;
-    }
-public void printDetails(){
-     System.out.println("Patient Name:" + patientName);
-     System.out.println("Patient Mobile:" + patientMobile);
-     System.out.println("Preferred Time Slot:" + preferredTimeSlot);
-     if (doctor !=null){
-         doctor.printDetails();
-     }else{
-         System.out.println("Doctor: Not assigned");
-     }
-}
-
-//Getter&Setter
-        public String getPatientName(){
-          return patientName;
-}
-     public void setPatientName(String patientName) {
-         this.patientName = patientName;
-}
-
-     public String getPatientMobile() {
-        return patientMobile;
-}
-
-    public void setPatientMobile(String patientMobile) {
-        this.patientMobile = patientMobile;
-}
-
-    public void setPreferredTimeSlot(String preferredTimeSlot) {
-        this.preferredTimeSlot = preferredTimeSlot;
-    }
-
-    public HealthProfessional getDoctor() {
-        return doctor;
-    }
-
-    public void setDoctor(HealthProfessional doctor) {
-        this.doctor = doctor;
-    }
-}
