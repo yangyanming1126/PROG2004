ad71e1dd158380d75bc1c3ee2a77b341c2d368bb
yangyanming1126 <y.yang.70@student.scu.edu.au>
2024-07-11 22:09:24 +0800

    improveGP1

--- yanmingyang-A1/src/GeneralPractitioner.java
+++ yanmingyang-A1/src/GeneralPractitioner.java
@@ -1,24 +1,34 @@
 public class GeneralPractitioner extends HealthProfessional {
     private String GeneralPractitioners;
 
-    public GeneralPractitioner(){
+    public GeneralPractitioner() {
         super();
         this.GeneralPractitioners = "General Doctor";
     }
-    public GeneralPractitioner(int id,String name, String department,String GeneralPractitioners) {
+
+    public GeneralPractitioner(int id, String name, String department, String GeneralPractitioners) {
         super(id, name, department);
         this.GeneralPractitioners = GeneralPractitioners;
     }
+
     @Override
-    public void printDetails(){
-        System.out.println("Details:");
+    public void printDetails() {
+        System.out.println("The doctor details are:");
         super.printDetails();
         System.out.println("GeneralPractitioners:" + GeneralPractitioners);
     }
-    public String getGeneralPractitioners(){
+
+    public String getGeneralPractitioners() {
         return GeneralPractitioners;
     }
+
     public void setGeneralPractitioners(String GeneralPractitioners) {
         this.GeneralPractitioners = GeneralPractitioners;
     }
+@Override
+public String toString(){
+    return "GeneralPractitioner{" +
+            "GeneralPractitioner='" + GeneralPractitioners + '\'' +
+            '}';
+}
 }
